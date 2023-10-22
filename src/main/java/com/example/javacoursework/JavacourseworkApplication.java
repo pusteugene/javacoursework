d40d40aaf685281633d9071f9cd79fb243f584c5
pusteugene <pustoviteugene@gmail.com>
2023-10-23 00:19:21 +0300

    Initial commit

---
+++ src/main/java/com/example/javacoursework/JavacourseworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.javacoursework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavacourseworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavacourseworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javacoursework/JavacourseworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javacoursework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavacourseworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
